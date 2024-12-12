fc30b822acda7d869b3437b9f38f6cd10778878e
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-12-12 17:14:49 -0300

    layout

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,6 +1,6 @@
 export const ThemeButton = ({children, changeTheme}) => {
     return (
-        <button onClick={changeTheme}>
+        <button onClick={changeTheme} className="border-2 rounded-full w-36 h-36 flex justify-center items-center">
             {children}
         </button>
     )
