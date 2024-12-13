caf0738c443ba5b6397d0df942c42b6e9090aef3
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2024-12-13 14:18:08 -0300

    adicionando transition

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -8,11 +8,11 @@ export const MainContent = () => {
     const [isDark, setIsDark] = useState(true);
 
     return (
-        <div className={`${isDark ? 'bg-slate-800' : 'bg-slate-100'} text-white flex flex-col justify-center items-center min-h-screen transition-all`}>
+        <div className={`${isDark ? 'bg-slate-800' : 'bg-slate-100'} text-white flex flex-col justify-center items-center min-h-screen transition-all ease-out duration-150`}>
             <div className="w-30 h-30">
                 <ThemeButton changeTheme={() => setIsDark(!isDark)} bgc={isDark ? '' : 'border-orange-300'}>
                     {
-                        isDark ? <FaMoon className="text-8xl fa-4x" /> : <FaSun className="text-8xl fa-4x text-orange-300" />
+                        isDark ? <FaMoon className="text-8xl fa-4x text-white" /> : <FaSun className="text-8xl fa-4x text-orange-300" />
                     }
                 </ThemeButton>
             </div>
